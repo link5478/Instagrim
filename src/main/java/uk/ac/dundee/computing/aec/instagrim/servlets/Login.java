3a98e7456bdcd01b1a73c9b803894213309e876d
carstencheyne <carstencheyne@labpc162.computing.dundee.ac.uk>
2016-10-20 20:42:34 +0100

    Added a button that will allow the user to log out by calling the LogOut.java servlet

--- src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/Login.java
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/Login.java
@@ -74,7 +74,7 @@ public class Login extends HttpServlet {
 
                 session.setAttribute("LoggedIn", lg);
                 System.out.println("Session in servlet "+session);
-                RequestDispatcher rd=request.getRequestDispatcher("/Instagrim/LoginSucess");
+                RequestDispatcher rd=request.getRequestDispatcher("loginsuccess.jsp");
                 rd.forward(request,response);
 
             }

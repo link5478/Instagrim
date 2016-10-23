2d3099929d560151566809afd36d458aee03b0ce
carstencheyne <carstencheyne@labpc162.computing.dundee.ac.uk>
2016-10-23 21:33:33 +0100

    Added code that would split up the URL supplied, and find out what, if anything, appeared after Profile/*, and input that into an attribute.
    Using this attribute it would be compared to the array list created in
    ProfileData.java, and find out the information where the username matches
    what was input into the URL.

--- src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/Profile.java
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/Profile.java
@@ -5,14 +5,19 @@
  */
 package uk.ac.dundee.computing.aec.instagrim.servlets;
 
+import com.datastax.driver.core.Cluster;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import uk.ac.dundee.computing.aec.instagrim.lib.CassandraHosts;
 import uk.ac.dundee.computing.aec.instagrim.lib.Convertors;
+import uk.ac.dundee.computing.aec.instagrim.models.ProfileData;
 
 /**
  *
@@ -21,15 +26,13 @@ import uk.ac.dundee.computing.aec.instagrim.lib.Convertors;
 @WebServlet(name = "Profile", urlPatterns = {"/Profile", "/Profile/*", "/profile", "/profile/*"})
 public class Profile extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    private Cluster cluster;
+
+    public void init(ServletConfig config) throws ServletException {
+        // TODO Auto-generated method stub
+        cluster = CassandraHosts.getCluster();
+    }
+
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -40,30 +43,25 @@ public class Profile extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         String args[] = Convertors.SplitRequestPath(request);
+        if (args.length < 3) {
+            RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
+            rd.forward(request, response);
+        }
+        request.setAttribute("user", args[2]);
 
-        request.setAttribute("username", args[2]);
+        ProfileData pdata = new ProfileData();
+        pdata.setCluster(cluster);
+        ArrayList<String> details = pdata.RetrieveDetails(args[2]);
+        if (details != null) {
+            request.setAttribute("details", details);
+        }
 
         RequestDispatcher rd = request.getRequestDispatcher("/profile.jsp");
         rd.forward(request, response);
-
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
     }
 
     /**
@@ -75,5 +73,4 @@ public class Profile extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
 }

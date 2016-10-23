50b136c3536b895a21eccf26e1471b3605b4833f
carstencheyne <carstencheyne@labpc162.computing.dundee.ac.uk>
2016-10-23 21:30:17 +0100

    Created a model that would search for the information as dictated in the code, in this case "select login, first_name, last_name from userprofiles where login =?); It would extract this information from the cassandra database, and add piece of information it finds into an ArrayList
    Error handling was added in the even that a field was empty in the
    table like no first name found, and would indicate there was no
    information in the arraylist.

---
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/models/ProfileData.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.dundee.computing.aec.instagrim.models;
+
+import com.datastax.driver.core.BoundStatement;
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author carstencheyne
+ */
+public class ProfileData {
+
+    Cluster cluster;
+
+    public ProfileData() {
+    }
+
+    public void setCluster(Cluster cluster) {
+        this.cluster = cluster;
+    }
+
+    public ArrayList<String> RetrieveDetails(String User) {
+        ArrayList<String> details = new ArrayList<>();
+        Session session = cluster.connect("instagrim");
+        PreparedStatement ps = session.prepare("select login, first_name, last_name from userprofiles where login =?");
+        ResultSet rs = null;
+        BoundStatement boundStatement = new BoundStatement(ps);
+        rs = session.execute( // this is where the query is executed
+                boundStatement.bind( // here you are binding the 'boundStatement'
+                        User));
+        if (rs.isExhausted() || rs == null) {
+            System.out.println("No details found");
+            return null;
+        } else {
+            for (Row row : rs) {
+                details.add(row.getString("login"));
+
+                if (row.getString("first_name") != null) {
+                    details.add(row.getString("first_name"));
+                } else {
+                    details.add("No Information Found");
+                }
+
+                if (row.getString("last_name") != null) {
+                    details.add(row.getString("last_name"));
+                } else {
+                    details.add("No Information Found");
+                }
+            }
+        }
+
+        return details;
+    }
+
+}
